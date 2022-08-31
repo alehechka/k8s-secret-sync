0a69935b390870dbb6a3fe1ffe522e12f1aa0ce7
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-08-30 21:18:56 -0500

    concurrently watch secrets and namespaces

--- client/sync.go
+++ client/sync.go
@@ -21,30 +21,41 @@ func SyncSecrets(config *SyncConfig) (err error) {
 		return err
 	}
 
-	watcher, err := clientset.CoreV1().Secrets(config.SecretsNamespace).Watch(ctx, metav1.ListOptions{})
+	secretWatcher, err := clientset.CoreV1().Secrets(config.SecretsNamespace).Watch(ctx, metav1.ListOptions{})
 	if err != nil {
 		return err
 	}
 
-	for event := range watcher.ResultChan() {
-		secret := event.Object.(*v1.Secret)
-
-		switch event.Type {
-		case watch.Added:
-			addSecrets(ctx, clientset, config, secret)
-		case watch.Modified:
-			modifySecrets(ctx, clientset, config, secret)
-		case watch.Deleted:
-			deleteSecrets(ctx, clientset, config, secret)
-		}
-
+	namespaceWatcher, err := clientset.CoreV1().Namespaces().Watch(ctx, metav1.ListOptions{})
+	if err != nil {
+		return err
 	}
 
-	return nil
+	for {
+		select {
+		case secretEvent := <-secretWatcher.ResultChan():
+			secretEventHandler(ctx, clientset, config, secretEvent)
+		case namespaceEvent := <-namespaceWatcher.ResultChan():
+			namespaceEventHandler(ctx, clientset, config, namespaceEvent)
+		}
+	}
+}
+
+func secretEventHandler(ctx context.Context, clientset *kubernetes.Clientset, config *SyncConfig, event watch.Event) {
+	secret := event.Object.(*v1.Secret)
+
+	switch event.Type {
+	case watch.Added:
+		addSecrets(ctx, clientset, config, secret)
+	case watch.Modified:
+		modifySecrets(ctx, clientset, config, secret)
+	case watch.Deleted:
+		deleteSecrets(ctx, clientset, config, secret)
+	}
 }
 
 func addSecrets(ctx context.Context, clientset *kubernetes.Clientset, config *SyncConfig, secret *v1.Secret) error {
-	log.Infof("[%s/%s]: Secret added", secret.ObjectMeta.Namespace, secret.ObjectMeta.Name)
+	log.Infof("[%s/%s]: Secret created", secret.ObjectMeta.Namespace, secret.ObjectMeta.Name)
 
 	return syncNamespaceSecret(ctx, clientset, config, secret, syncAddedModifiedSecret)
 }
@@ -59,8 +70,21 @@ func modifySecrets(ctx context.Context, clientset *kubernetes.Clientset, config 
 	return syncNamespaceSecret(ctx, clientset, config, secret, syncAddedModifiedSecret)
 }
 
-func deleteSecrets(ctx context.Context, clientset *kubernetes.Clientset, config *SyncConfig, secret *v1.Secret) {
+func deleteSecrets(ctx context.Context, clientset *kubernetes.Clientset, config *SyncConfig, secret *v1.Secret) error {
 	log.Infof("[%s/%s]: Secret deleted", secret.ObjectMeta.Namespace, secret.ObjectMeta.Name)
 
-	syncNamespaceSecret(ctx, clientset, config, secret, syncDeletedSecret)
+	return syncNamespaceSecret(ctx, clientset, config, secret, syncDeletedSecret)
+}
+
+func namespaceEventHandler(ctx context.Context, clientset *kubernetes.Clientset, config *SyncConfig, event watch.Event) {
+	namespace := event.Object.(*v1.Namespace)
+
+	switch event.Type {
+	case watch.Added:
+		addNamespace(ctx, clientset, config, namespace)
+	}
+}
+
+func addNamespace(ctx context.Context, clientset *kubernetes.Clientset, config *SyncConfig, namespace *v1.Namespace) {
+	log.Infof("[%s]: Namespace created", namespace.Name)
 }
