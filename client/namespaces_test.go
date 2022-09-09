0bed7b7d2da3a1d0541d9f384be4f2371c5a0b2b
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-09-08 20:22:56 -0500

    move test vars/constants

--- client/namespaces_test.go
+++ client/namespaces_test.go
@@ -6,26 +6,15 @@ import (
 	typesv1 "github.com/alehechka/kube-secret-sync/api/types/v1"
 
 	"github.com/stretchr/testify/assert"
-	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-const (
-	keyTestNamespace string = "test-namespace"
-	keyTestSecret    string = "test-secret"
-	keyDefault       string = "default"
-)
-
-var testNamespace = &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: keyTestNamespace}}
-var defaultNamespace = &v1.Namespace{ObjectMeta: metav1.ObjectMeta{Name: keyDefault}}
-var testSecret = &v1.Secret{ObjectMeta: metav1.ObjectMeta{Name: keyTestSecret, Namespace: keyDefault}}
 
 func Test_SyncSecretToNamespace(t *testing.T) {
 	client := InitializeTestClientset()
 
-	client.CreateSecret(defaultNamespace, testSecret)
+	client.CreateSecret(defaultNamespace, defaultSecret)
 
-	err := client.SyncSecretToNamespace(testNamespace, &typesv1.SecretSyncRule{Spec: typesv1.SecretSyncRuleSpec{Secret: keyTestSecret, Namespace: keyDefault}})
+	err := client.SyncSecretToNamespace(testNamespace, &typesv1.SecretSyncRule{Spec: typesv1.SecretSyncRuleSpec{Secret: keyDefaultSecret, Namespace: keyDefault}})
 
 	assert.NoError(t, err)
 }
@@ -33,7 +22,7 @@ func Test_SyncSecretToNamespace(t *testing.T) {
 func Test_SyncSecretToNamespace_NoSecret(t *testing.T) {
 	client := InitializeTestClientset()
 
-	err := client.SyncSecretToNamespace(testNamespace, &typesv1.SecretSyncRule{Spec: typesv1.SecretSyncRuleSpec{Secret: keyTestSecret, Namespace: keyDefault}})
+	err := client.SyncSecretToNamespace(testNamespace, &typesv1.SecretSyncRule{Spec: typesv1.SecretSyncRuleSpec{Secret: keyDefaultSecret, Namespace: keyDefault}})
 
 	assert.Error(t, err)
 }
